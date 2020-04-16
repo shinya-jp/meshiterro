3442d9c76fe6b877ae8eebf899f26711e602e7ca
shinya-jp <shinya0733@gmail.com>
2020-04-17 01:22:51 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd7dd2b8b51ed48cb7c32064588dd32a7a628414e67c3eb92b116cf6aec29362635eddf0b6b1bf2beab5157b4d5dc8f3be71efaa028637a6421e10562e1f03077'
